1fa8a48070fe34c2f569857fdeba357f19b684ee
Jarrhedjamm <jm13@kent.ac.uk>
2026-03-27 08:55:36 +0000

    fixed the 7 mistake that were in my code mostly stuff like upper case letters or me doing elif opt == "5" as elif == opt "5"

--- miniproject2.py
+++ miniproject2.py
@@ -1,20 +1,21 @@
-name = ["sett", "Heimerdinger", "sion", "ornn"]
+name = ["sett", "Heimerdinger", "sion", "ornn", "vi"]
 role = ["manager", "kitchen", "server", "bar", "washer"]
-wage = ["25.50", "20.50", "13.50", "18.50", "11.50"]
+wage = [25.50, 20.50, 13.50, 18.50, 11.50]
 
-while true:
+while True:
     print("\n---Restaurant management---")
     print("1.view staff")
     print("2.Add staff")
     print("3.remove staff")
     print("4.payroll")
     print("5.exit")
+    print("6.exit")
 
     opt = input ("\nselect your option: ")
     
     if opt == "1":
         print("\n---staff list---")
-        for i in range(len(names)):
+        for i in range(len(name)):
             print(str(i+1) + ". " + name[i] + " - " + role[i] + " (£"+ str(wage[i]) + "/hr)")
 
     elif opt == "2":
@@ -43,8 +44,20 @@ while true:
         print("The total cost per hour for all staff is: £" + str(total))
 
     elif opt == "5":
+        search_name = input("enter the name of the staff: ")
+
+        if search_name in name:
+            idx = name.index(search_name)
+            print("\n---staff member found---")
+            print("name: " +name[idx])
+            print("role: " + role[idx])
+            print("hourly wage: £" + str(wage[idx]))
+        else:
+            print("error: '" + search_name + "' is not in the system.")
+
+    elif opt == "6":
         print("exiting")
         break
 
     else:
-        print("invalid option. choose from options 1, 2, 3, 4, 5")
+        print("invalid option. choose from options 1, 2, 3, 4, 5, 6")
