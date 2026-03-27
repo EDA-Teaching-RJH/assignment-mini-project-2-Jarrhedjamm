b695b3216ea7590ec46e10036a226d3ee6f39422
Jarrhedjamm <jm13@kent.ac.uk>
2026-03-27 08:39:59 +0000

    second commit made it so with option 1 it will just read out the name role and wage of the staff

--- miniproject2.py
+++ miniproject2.py
@@ -1,6 +1,6 @@
 name = ["sett", "Heimerdinger", "sion", "ornn"]
-roles = ["manager", "kitchen", "server", "bar", "washer"]
-wages = ["25.50", "20.50", "13.50", "18.50", "11.50"]
+role = ["manager", "kitchen", "server", "bar", "washer"]
+wage = ["25.50", "20.50", "13.50", "18.50", "11.50"]
 
 while true:
     print("\n---Restaurant management---")
@@ -11,3 +11,10 @@ while true:
     print("5.exit")
 
     opt = input ("\nselect your option: ")
+    
+    if opt == "1":
+        print("\n---staff list---")
+        for i in range(len(names)):
+            print(str(i+1) + ". " + name[i] + " - " + role[i] + " (£"+ str(wage[i]) + "/hr)")
+
+    
