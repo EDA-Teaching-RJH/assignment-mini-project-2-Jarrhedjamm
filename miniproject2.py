5babe802e5d93e6bf81e583dcdf5318998950bb3
Jarrhedjamm <jm13@kent.ac.uk>
2026-03-27 07:57:16 +0000

    first commit ,have the base down for the code with the option list and the names/roles/wages of the people at the restaurant

---
+++ miniproject2.py
@@ -0,0 +1,13 @@
+name = ["sett", "Heimerdinger", "sion", "ornn"]
+roles = ["manager", "kitchen", "server", "bar", "washer"]
+wages = ["25.50", "20.50", "13.50", "18.50", "11.50"]
+
+while true:
+    print("\n---Restaurant management---")
+    print("1.view staff")
+    print("2.Add staff")
+    print("3.remove staff")
+    print("4.payroll")
+    print("5.exit")
+
+    opt = input ("\nselect your option: ")
